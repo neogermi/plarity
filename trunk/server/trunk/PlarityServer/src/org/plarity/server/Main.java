40d59d32b91e3e8f3462a9b80a95d6326ddddd9e
neogermi@gmail.com <neogermi@gmail.com@f1617eed-8401-374f-ed9e-c73b3086b98f>
2010-12-30 16:17:24 +0000

    added support for sending emails

--- trunk/server/trunk/PlarityServer/src/org/plarity/server/Main.java
+++ trunk/server/trunk/PlarityServer/src/org/plarity/server/Main.java
@@ -12,6 +12,7 @@ import java.util.logging.Logger;
 
 import org.plarity.server.database.IDatabase;
 import org.plarity.server.database.SQLite;
+import org.plarity.server.mail.Mail;
 import org.plarity.server.user.UserCenter;
 
 public class Main {
@@ -20,9 +21,16 @@ public class Main {
 
 	public static String dbName = "";
 
-	public static String port = "";
+	public static String severPort = "";
+
+	public static String mailServerHost = "";
+	public static String mailServerPort = "";
+	public static String mailAddress = "";
+	public static String mailUser = "";
+	public static String mailPassword = "";
 
 	public static IDatabase database;
+	public static Mail mail;
 	public static UserCenter userCenter;
 
 	public static void main(String[] args) throws IOException, SQLException {
@@ -32,7 +40,17 @@ public class Main {
 			if (arg.equalsIgnoreCase("-n")) {
 				dbName = args[++i];
 			} else if (arg.equalsIgnoreCase("-p")) {
-				port = args[++i];
+				severPort = args[++i];
+			} else if (arg.equalsIgnoreCase("-msh")) {
+				mailServerHost = args[++i];
+			} else if (arg.equalsIgnoreCase("-msp")) {
+				mailServerPort = args[++i];
+			} else if (arg.equalsIgnoreCase("-ma")) {
+				mailAddress = args[++i];
+			} else if (arg.equalsIgnoreCase("-mu")) {
+				mailUser = args[++i];
+			} else if (arg.equalsIgnoreCase("-mp")) {
+				mailPassword = args[++i];
 			}
 		}
 
@@ -41,8 +59,37 @@ public class Main {
 			System.exit(1);
 		}
 
-		if (port.equalsIgnoreCase("")) {
+		if (severPort.equalsIgnoreCase("")) {
 			LOGGER.log(Level.SEVERE, "Please provide a valid port for the REST server, using the -p <port> option!");
+			System.exit(1);
+		}
+
+		if (mailServerHost.equalsIgnoreCase("")) {
+			LOGGER.log(Level.SEVERE,
+					"Please provide a valid port for the mail server, using the -msh <smtp.example.com> option!");
+			System.exit(1);
+		}
+
+		if (mailServerPort.equalsIgnoreCase("")) {
+			LOGGER.log(Level.SEVERE, "Please provide a valid port for the mail server, using the -msp <port> option!");
+			System.exit(1);
+		}
+
+		if (mailAddress.equalsIgnoreCase("")) {
+			LOGGER.log(Level.SEVERE,
+					"Please provide a valid port for the mail server, using the -ma <you@example.com> option!");
+			System.exit(1);
+		}
+
+		if (mailUser.equalsIgnoreCase("")) {
+			LOGGER.log(Level.SEVERE,
+					"Please provide a valid port for the mail server, using the -mu <username> option!");
+			System.exit(1);
+		}
+
+		if (mailPassword.equalsIgnoreCase("")) {
+			LOGGER.log(Level.SEVERE,
+					"Please provide a valid port for the mail server, using the -mp <password> option!");
 			System.exit(1);
 		}
 
@@ -53,15 +100,19 @@ public class Main {
 			System.exit(2);
 		}
 
+		//setting up mail service
+		mail = new Mail(mailServerHost, mailServerPort, mailAddress, mailUser, mailPassword);
+
 		//setting up user center
-		userCenter = new UserCenter(database);
+		userCenter = new UserCenter(database, mail);
 
 		//creating RESTful endpoint
-		final String baseUri = "http://localhost:" + port + "/";
+		final String baseUri = "http://localhost:" + severPort + "/";
 		final Map<String, String> initParams = new HashMap<String, String>();
 		initParams.put("com.sun.jersey.config.property.packages", "org.plarity.server.resource");
 		SelectorThread threadSelector = GrizzlyWebContainerFactory.create(baseUri, initParams);
 
+		LOGGER.info("Server is up an running!");
 		//press the 'any key' button to quit the program
 		System.in.read();
 		threadSelector.stopEndpoint();
