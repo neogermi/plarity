fa7eed9c1ab72a9c1d2ba2d0d6abd9859b35e859
neogermi@gmail.com <neogermi@gmail.com@f1617eed-8401-374f-ed9e-c73b3086b98f>
2011-01-05 17:44:55 +0000

    added first version of sign up dialog

--- trunk/client/PlarityWebClient/trunk/js/plarity.js
+++ trunk/client/PlarityWebClient/trunk/js/plarity.js
@@ -3,6 +3,25 @@
  */
 
 function openSignUp() {
+	var dialogForm = $("#dialog-form");
 	
-	alert("sign up!");
+	if (dialogForm) {
+		dialogForm.dialog({
+			autoOpen: true,
+			height: 300,
+			width: 350,
+			modal: true,
+			buttons: {
+				"Sign up!": function() {
+					//TODO!
+				},
+				"Cancel": function() {
+					$( this ).dialog( "close" );
+				}
+			},
+			close: function() {
+				//TODO!
+			}
+		});
+	}
 }
